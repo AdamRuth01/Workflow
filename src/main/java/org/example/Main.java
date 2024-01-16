1c9cda4f1d9063e724753196317dd1b78d615442
Adam Ruth <adam.ruth01@gmail.com>
2024-01-16 14:35:50 +0100

    Update action

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!Jag Heter Adam Ruth");
+        System.out.println("Hello world!I like SSK");
+        System.out.println("Most people do make it");
     }
 }
